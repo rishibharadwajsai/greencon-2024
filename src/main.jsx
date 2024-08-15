bb70e12aa6c92995a1ef48ffbdc85acdbe42f85d
StephenStolk <shikharburman2@gmail.com>
2024-08-15 18:31:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,34 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import MobNavbar from './components/MobNavbar.jsx'
-import Navbar from './components/Navbar.jsx'
-
-function Main() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  }, []);
-
-  return (
-    <>
-      {isMobile ? <MobNavbar /> : <Navbar />}
-      <App />
-    </>
-  )
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Main />
+    <MobNavbar />
+    <App />
   </StrictMode>,
 )
