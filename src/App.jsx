b088666fb25dae897d0816a4a6cd2db58c7a1f80
StephenStolk <shikharburman2@gmail.com>
2024-08-15 18:24:02 +0530

    Added Navbar component for Mobile Screens.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import MobNavbar from './components/MobNavbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
     </>
   )
 }
