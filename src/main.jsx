e0e5f7c1d9f9300a0b78b5eb5f84d803e355d521
StephenStolk <shikharburman2@gmail.com>
2024-08-15 18:33:37 +0530

    Toggling Effect

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,37 @@
+/* eslint-disable react-refresh/only-export-components */
 import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import MobNavbar from './components/MobNavbar.jsx'
+import Navbar from './components/Navbar.jsx'
+
+function Main() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  return (
+    <>
+      {isMobile ? <MobNavbar /> : <Navbar />}
+      <App />
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MobNavbar />
-    <App />
+    <Main />
   </StrictMode>,
 )
