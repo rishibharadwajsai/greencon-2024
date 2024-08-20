d5ee2683131da13b5d8167d982559ec291efcbc9
StephenStolk <shikharburman2@gmail.com>
2024-08-20 22:42:26 +0530

    Updated Timeline Component

--- src/components/home/Timeline.jsx
+++ src/components/home/Timeline.jsx
@@ -27,17 +27,17 @@ const timelineData = [
 const Timeline = () => {
   return (
     <div className="relative max-w-2xl mx-auto mt-10 px-4 mb-12">
-      <div className="absolute border-l-2 border-blue-500 h-full left-6 top-0"></div>
+      <div className="absolute border-l-2 border-[#3c5a96] h-full left-6 top-0"></div>
       <ul className="space-y-6">
         {timelineData.map((item, index) => (
           <li key={index} className="relative pl-8">
-            <div className="absolute top-6 left-0 w-4 h-4 bg-blue-500 rounded-full mt-1.5"></div>
+            <div className="absolute top-6 left-0 w-4 h-4 bg-[#3c5a96] rounded-full mt-1.5"></div>
             <div className="bg-white shadow-lg rounded-lg p-4 border border-gray-200 ">
               <h3 className="text-sm font-semibold text-gray-500">
                 {item.date}
                 {item.endDate ? ` - ${item.endDate}` : ""}
               </h3>
-              <p className="mt-2 text-blue-800 font-medium">{item.title}</p>
+              <p className="mt-2 text-[#3c5a96] font-medium">{item.title}</p>
             </div>
           </li>
         ))}
