271a164561d22a1601bd02e17c9e108fd364af0d
StephenStolk <shikharburman2@gmail.com>
2024-08-21 18:38:16 +0530

    Updated Timeline

--- src/components/home/Timeline.jsx
+++ src/components/home/Timeline.jsx
@@ -26,23 +26,28 @@ const timelineData = [
 
 const Timeline = () => {
   return (
-    <div className="relative max-w-2xl mx-auto mt-10 px-4 mb-12">
-      <div className="absolute border-l-2 border-[#3c5a96] h-full left-6 top-0"></div>
+    <>
+    <div className="relative max-w-2xl mx-auto mt-10 px-4 mb-12 py-2">
+      <div className="absolute border-l-2 border-green-800 h-full left-6 top-0"></div>
       <ul className="space-y-6">
         {timelineData.map((item, index) => (
           <li key={index} className="relative pl-8">
-            <div className="absolute top-6 left-0 w-4 h-4 bg-[#3c5a96] rounded-full mt-1.5"></div>
-            <div className="bg-white shadow-lg rounded-lg p-4 border border-gray-200 ">
+            <div className="absolute top-6 left-0 w-4 h-4 bg-green-900 rounded-full mt-1.5"></div>
+            <div className="bg-gray-50 shadow-lg p-4 border border-green-900 rounded-br-[1rem] rounded-tl-xl hover:cursor-pointer hover:bg-white">
               <h3 className="text-sm font-semibold text-gray-500">
                 {item.date}
                 {item.endDate ? ` - ${item.endDate}` : ""}
               </h3>
-              <p className="mt-2 text-[#3c5a96] font-medium">{item.title}</p>
+              <p className="mt-2 text-[#35575a] font-medium">{item.title}</p>
             </div>
           </li>
         ))}
       </ul>
     </div>
+    <div className=''>
+
+    </div>
+    </>
   );
 };
 
